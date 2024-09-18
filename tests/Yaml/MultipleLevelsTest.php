78b56998a0eeff46bef37a9f9693933d89e913c3
Diego Brocanelli <diegod2@msn.com>
2024-09-17 23:06:36 -0300

    Implementação dos testes para leitura e parser de arquivo yaml com múltiplos níveis de informações

---
+++ tests/Yaml/MultipleLevelsTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NucleusFramework\Configuration\Tests\Yaml;
+
+use NucleusFramework\Configuration\Adapters\Yaml\Yaml;
+use NucleusFramework\Configuration\Reader;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+final class MultipleLevelsTest extends TestCase
+{
+    public function testFileNotFound(): void
+    {
+        $this->expectException(RuntimeException::class);
+
+        $this->expectExceptionMessage('Configudation file not nound');
+
+        $read = new Reader(new Yaml());
+        $read->readFromFile('/invalid/path/invalid-file.yaml');
+    }
+
+    public function testReadFile(): void
+    {
+        $reader = new Reader(new Yaml());
+        $reader->readFromFile(__DIR__ . '/multiple-levels.yaml');
+
+        $configurationList = $reader->collectAll();
+
+        $this->assertTrue($configurationList !== []);
+    }
+
+    public function testReadAppInfo(): void
+    {
+        $reader = new Reader(new Yaml());
+        $reader->readFromFile(__DIR__ . '/multiple-levels.yaml');
+
+        $configurationList = $reader->collectAll();
+
+        $this->assertArrayHasKey('app', $configurationList);
+
+        $appInfo = $configurationList['app'];
+        $this->assertEquals('MyAwesomeApp', $appInfo['name']);
+        $this->assertEquals('1.0.0', $appInfo['version']);
+        $this->assertEquals(
+            'This is a sample configuration file for MyAwesomeApp.',
+            $appInfo['description']
+        );
+        $this->assertEquals('MIT', $appInfo['license']);
+    }
+
+    public function testReadDatabaseInfo(): void
+    {
+        $reader = new Reader(new Yaml());
+        $reader->readFromFile(__DIR__ . '/multiple-levels.yaml');
+
+        $configurationList = $reader->collectAll();
+
+        $databaseInfo = $configurationList['database'];
+        $this->assertArrayHasKey('database', $configurationList);
+        $this->assertEquals('postgres', $databaseInfo['type']);
+        $this->assertEquals('localhost', $databaseInfo['host']);
+        $this->assertEquals('5432', $databaseInfo['port']);
+        $this->assertEquals('admin', $databaseInfo['username']);
+        $this->assertEquals('password', $databaseInfo['password']);
+        $this->assertEquals('my_database', $databaseInfo['name']);
+    }
+
+    public function testReadServerInfo(): void
+    {
+        $reader = new Reader(new Yaml());
+        $reader->readFromFile(__DIR__ . '/multiple-levels.yaml');
+
+        $configurationList = $reader->collectAll();
+
+        $this->assertArrayHasKey('server', $configurationList);
+
+        $serverInfo = $configurationList['server'];
+        $this->assertEquals('localhost', $serverInfo['host']);
+        $this->assertEquals('8080', $serverInfo['port']);
+        $this->assertEquals('true', $serverInfo['debug']);
+
+        $this->assertArrayHasKey('logging', $serverInfo);
+
+        $loggingInfo = $serverInfo['logging'];
+        $this->assertEquals('info', $loggingInfo['level']);
+        $this->assertEquals('/var/log/myawesomeapp.log', $loggingInfo['file']);
+
+        $this->assertArrayHasKey('rotate', $loggingInfo);
+
+        $rotate = $loggingInfo['rotate'];
+        $this->assertEquals('10MB', $rotate['max_size']);
+        $this->assertEquals('5', $rotate['max_files']);
+    }
+}
